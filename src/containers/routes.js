ce14b8b395f6bbcf76c9c801d517ef5e69d1fa6d
Jennifer Perez <jenniferperez@pursuit.org>
2019-06-16 13:53:39 -0400

    css

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -27,10 +27,10 @@ componentDidMount(){
   render() {
     console.log('App Context', this.context)
     const loggedOutRoutes = <Switch>
-      <Route path="/" exact component={Opening} /> 
+      <Route path="/opening" exact component={Opening} /> 
       <Route path="/signup" exact component={Signup} />
       <Route path="/login" exact component={Login} />
-      <Route exact component={Error404} />
+      <Route path="/logout" exact  strict component={Logout} />
     </Switch>;
 
     const loggedInRoutes = <Switch>
@@ -39,7 +39,11 @@ componentDidMount(){
       <Route path="/mix-N-match" exact strict component={MixNMatch} />
       <Route path="/ootd" exact strict component={Ootd} />
       <Route path="/calendar" exact strict component={BigCalendar} /> 
-      <Route path="/logout" exact  strict component={Logout} />
+      
+      <Route path="/opening" exact strict component={() => null} /> 
+      <Route path="/signup" exact strict component={() => null} /> 
+      <Route path="/login" exact strict component={() => null} /> 
+      <Route exact component={Error404} />
     </Switch>
 
     return (
